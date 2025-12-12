44ed04e04325d6770ce1e697a965520ce59b66e0
calvin <calvinbergin212@gmail.com>
2025-12-12 21:30:50 +0000

    Refactor Astro configuration and update dependencies
    - Removed Tailwind CSS Vite plugin from Astro configuration.
    - Added PostCSS and Autoprefixer as dev dependencies.
    - Updated package-lock.json to reflect changes in dependencies.
    - Updated global CSS to import Tailwind CSS.
    - Modified footer and header components to link to personal social media accounts.
    - Adjusted publication dates in blog posts for future scheduling.
    - Updated author information in blog post for clarity.
    
    Signed-off-by: calvin <calvinbergin212@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sso.ie',
@@ -15,8 +13,5 @@ export default defineConfig({
       theme: 'dracula',
       wrap: true
     }
-  },
-  vite: {
-    plugins: [tailwindcss()],
   },
 });
