d47ae40cc9c29659afe7447c114792a830fec149
calvin <calvinbergin212@gmail.com>
2025-12-12 21:16:31 +0000

    Update site title and author details in index.astro for consistency and clarity
    Signed-off-by: calvin <calvinbergin212@gmail.com>

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Site metadata
-export const SITE_TITLE = 'Calvins ZBlog';
+export const SITE_TITLE = 'Calvins Blog';
 export const SITE_DESCRIPTION = 'Research, automation, and stuff.';
