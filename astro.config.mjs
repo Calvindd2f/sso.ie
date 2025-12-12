8adcea9cc009e74dd4fc9e519e5b939e26b790c1
calvin <calvinbergin212@gmail.com>
2025-12-12 21:06:49 +0000

    Initial blog setup - sso.ie

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://sso.ie',
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      wrap: true
+    }
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// Site metadata
+export const SITE_TITLE = 'Calvins ZBlog';
+export const SITE_DESCRIPTION = 'Research, automation, and stuff.';
